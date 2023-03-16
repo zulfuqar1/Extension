02bf792bdd393090e2ff9c418131fb2687daca8e
zulfuqar1 <zulfugarga@code.edu.az>
2023-03-16 12:39:38 +0400

    extension

---
+++ extention/Program.cs
@@ -0,0 +1,6 @@
+﻿using extention;
+
+string input = "Salam zulu. Necəsən zulu,";
+string pattern = "zulu";
+int count = input.MatchCount(pattern);
+Console.WriteLine(count);
---
+++ extention/extension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extention;
+
+public static class StringExtensions
+{
+    public static int MatchCount(this string str, string pattern)
+    {
+        int count = 0;
+
+        int i = 0;
+
+        while ((i = str.IndexOf(pattern, i)) != -1)
+        {
+            i += pattern.Length;
+            count++;
+        }
+
+        return count;
+
+    }
+}
